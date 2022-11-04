b2df82ccbb117db2b8fe9762066abb3469e8ec84
= <=>
2022-11-04 09:27:29 -0300

    using nohup to run spark batch processor; logging flume job

--- flume_setup/agent-run.sh
+++ flume_setup/agent-run.sh
@@ -1,8 +1,11 @@
 sudo -u hdfs hadoop fs -mkdir -p /user/custom
 sudo -u hdfs hadoop fs -chmod 777 /user/custom
 
+mkdir /home/ec2-user/weatheranalysis/logs -p
+
 setsid nohup /opt/flume/bin/flume-ng agent \
 --conf-file /home/ec2-user/weatheranalysis/flume_setup/weather-ingestion-flume-agent.conf \
 --name weather_agent \
 --conf /opt/flume/conf/ -Xms1024m -Xmx2048m \
--Dflume.root.logger=INFO,console
+-Dflume.root.logger=INFO,console \
+ 1> /home/ec2-user/weatheranalysis/logs/flume.out_1 2> /home/ec2-user/weatheranalysis/logs/flume.out_2 < /dev/null &
--- producing-data.sh
+++ producing-data.sh
@@ -1,11 +1,13 @@
 # local environment variables
 S3_TRANSIENT_BUCKET_NAME=weatheranalysistransient
 S3_DATA_DATA_BUCKET_NAME=weatheranalysisdata
-MONGODB_ENDPOINT=mongodb://root:root2022@weather-analysis-db.cluster-c4se9etyjkgh.us-east-1.docdb.amazonaws.com:27017/weather
+MONGODB_ENDPOINT=mongodb://root:root2022@weather-analysis-documentdb.cluster-c4se9etyjkgh.us-east-1.docdb.amazonaws.com:27017/weather
 HDFS_HOSTNAME_PORT=${HOSTNAME}.ec2.internal:8020
+REFERENCE_DATE=2022-11-04
 
 # download setup files from s3
 mkdir /home/ec2-user/weatheranalysis
+mkdir /home/ec2-user/weatheranalysis/logs
 cd /home/ec2-user/weatheranalysis
 aws s3 cp --recursive s3://$S3_TRANSIENT_BUCKET_NAME ./
 
@@ -25,7 +27,7 @@ java -jar ./java/kafka-producer-0.0.1.jar --separator=, --file=file:///home/ec2-
 java -jar ./java/kafka-producer-0.0.1.jar --separator=, --file=file:///home/ec2-user/weatheranalysis/dataset/wind_speed.csv
 
 # starting batch processor
-sudo spark-submit \
+nohup sudo spark-submit \
 ./java/sparkbatchprocessing-0.0.1-jar-with-dependencies.jar \
 --spark_master=local[*] \
 --weatherapp.hdfs.base_path=hdfs://$HDFS_HOSTNAME_PORT/user/custom/weather_data \
@@ -35,5 +37,6 @@ sudo spark-submit \
 --weatherapp.mongodb.replicaSet=rs0 \
 --weatherapp.mongodb.readPreference=secondaryPreferred \
 --weatherapp.mongodb.retryWrites=false \
---weatherapp.reference_date=2022-11-03 \
---weatherapp.process_only=city_attributes,weather_description,humidity,pressure,temperature,wind_direction,wind_speed
+--weatherapp.reference_date=$REFERENCE_DATE \
+--weatherapp.process_only=city_attributes,weather_description,humidity,pressure,temperature,wind_direction,wind_speed \
+1> ./logs/sparkbatchprocessing.out_1 2> ./logs/sparkbatchprocessing.out_2 < /dev/null &
